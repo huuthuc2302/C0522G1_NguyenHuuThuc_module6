c4e48e4232452b591d4ae122b45c3e10b3342a90
Your Name <you@example.com>
2022-12-01 17:05:41 +0700

    01/12

--- Spring2/FE/lego-manager-fe/src/app/app.module.ts
+++ Spring2/FE/lego-manager-fe/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ListComponent } from './list/list.component';
+import { CartComponent } from './cart/cart.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ListComponent,
+    CartComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
